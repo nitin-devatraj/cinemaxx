e60935d6023d766c3a4a077b8c368a591933b922
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-12 01:48:52 +0530

    create displayPopularMovies func to display popular movies

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", init);
+
 const global = {
   currentPage: window.location.pathname,
 };
@@ -8,6 +10,7 @@ function init() {
     case "/":
     case "/index.html":
       console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
@@ -26,6 +29,39 @@ function init() {
   highlightActiveLink();
 }
 
+// fetch popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchApiData("movie/popular");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            ${
+              movie.poster_path
+                ? `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title}"
+              />`
+                : `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>  
+    `;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
 // Highlight the active link (movies || tv shows)
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -36,17 +72,26 @@ function highlightActiveLink() {
   });
 }
 
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
 // fetch data from the TMDB API
 async function fetchApiData(endPoint) {
   const API_KEY = "493416f28c67cc7b85eaf63fa3006630";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
   const response = await fetch(
     `${API_URL}${endPoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
+  hideSpinner();
+
   return data;
 }
-
-document.addEventListener("DOMContentLoaded", init);
