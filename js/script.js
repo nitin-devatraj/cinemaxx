68851f9210f05a9be9501c1b2d1f58a15c5fa263
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-12 04:18:15 +0530

    create displayMovieSlider func to show movie slider on homepage

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
+      displayMovieSlider();
       displayPopularMovies();
       break;
     case "/shows.html":
@@ -21,7 +22,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
 
@@ -229,6 +230,49 @@ async function displayShowDetails() {
       </div>
     `;
   document.querySelector("#show-details").appendChild(div);
+}
+
+// display movies slider
+async function displayMovieSlider() {
+  const { results } = await fetchApiData("movie/now_playing");
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4> 
+        `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
 }
 
 // Highlight the active link (movies || tv shows)
