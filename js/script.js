5dc95e5a2a3e6b9945b4ee10ac072833b02e2d1d
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-12 02:01:08 +0530

    create displayPopularShows func to display popular shows

--- js/script.js
+++ js/script.js
@@ -9,11 +9,10 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
       displayPopularMovies();
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayPopularShows();
       break;
     case "/movie-details.html":
       console.log("Movie Details");
@@ -29,7 +28,7 @@ function init() {
   highlightActiveLink();
 }
 
-// fetch popular movies
+// display popular movies
 async function displayPopularMovies() {
   const { results } = await fetchApiData("movie/popular");
   results.forEach((movie) => {
@@ -59,6 +58,41 @@ async function displayPopularMovies() {
           </div>  
     `;
     document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// display popular tv shows
+async function displayPopularShows() {
+  const { results } = await fetchApiData("tv/popular");
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+            <a href="tv-details.html?id=${show.id}">
+              ${
+                show.poster_path
+                  ? `<img
+                  src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                  class="card-img-top"
+                  alt="${show.name}"
+                />`
+                  : `<img
+                src="../images/no-image.jpg"
+                class="card-img-top"
+                alt="${show.name}"
+              />`
+              }
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">${show.name}</h5>
+              <p class="card-text">
+                <small class="text-muted">Air Date: ${
+                  show.first_air_date
+                }</small>
+              </p>
+            </div>  
+      `;
+    document.querySelector("#popular-shows").appendChild(div);
   });
 }
 
