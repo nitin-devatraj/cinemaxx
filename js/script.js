08ffb239995f03086c73ed5f76009a5ca84a0d81
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-12 02:40:08 +0530

    create displayMovieDetails func to display movie details

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function init() {
       displayPopularShows();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
@@ -96,6 +96,71 @@ async function displayPopularShows() {
   });
 }
 
+// display movie details
+async function displayMovieDetails() {
+  const movieID = window.location.search.split("=")[1];
+  const movie = await fetchApiData(`movie/${movieID}`);
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+              : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+             ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href=${
+              movie.homepage
+            } target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumbers(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumbers(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(",  ")}</div>
+    </div>
+  `;
+  document.querySelector("#movie-details").appendChild(div);
+}
+
 // Highlight the active link (movies || tv shows)
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -112,6 +177,10 @@ function showSpinner() {
 
 function hideSpinner() {
   document.querySelector(".spinner").classList.remove("show");
+}
+
+function addCommasToNumbers(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // fetch data from the TMDB API
